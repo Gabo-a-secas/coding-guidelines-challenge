342a6bce688b1ba35c3cc437011111ee9c52e732
Gabriel Andrés Gandarillas Mendoza <gbr_360@hotmail.com>
2025-05-17 10:25:17 +0000

    first commit

--- src/02-js-variable-names.js
+++ src/02-js-variable-names.js
@@ -1,12 +1,12 @@
-function calc(nums) {
-    let total = 0;
-    for (let i = 0; i < nums.length; i++) {
-      if (nums[i] > 0) {
-        total += nums[i];
-      }
+function sumPositiveNumbers(nums) {
+  let positiveSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      positiveSum += nums[i];
     }
-    return total;
   }
+  return positiveSum;
+}
   
-  const numbers = [1, -2, 3, -4, 5, -6, 7, -8, 9, -10];
-  console.log(calc(numbers));
+const numbers = [1, -2, 3, -4, 5, -6, 7, -8, 9, -10];
+console.log(sumPositiveNumbers(numbers));
